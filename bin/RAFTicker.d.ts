e079d5a8587745393f9189f2353be7dc56b6dc60
MasatoMakino <unagiinu.g@gmail.com>
2019-11-16 17:26:20 +0900

    update : docs and bim

--- bin/RAFTicker.d.ts
+++ bin/RAFTicker.d.ts
@@ -1,11 +1,12 @@
+import { RAFTickerEvent } from "./RAFTickerEvent";
 export declare class RAFTicker {
     private static _dispatcher;
     private static _lastUpdateTimestamp;
     protected static _id: number;
     static initialize(): void;
-    static addEventListener(type: string, listener: (event: Event) => void): void;
-    static hasEventListener(type: string, listener: (event: Event) => void): boolean;
-    static removeEventListener(type: string, listener: (event: Event) => void): void;
+    static addEventListener(type: string, listener: (event: RAFTickerEvent) => void): void;
+    static hasEventListener(type: string, listener: (event: RAFTickerEvent) => void): boolean;
+    static removeEventListener(type: string, listener: (event: RAFTickerEvent) => void): void;
     private static onTick;
 }
 //# sourceMappingURL=RAFTicker.d.ts.map
