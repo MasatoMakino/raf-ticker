868a029f01a120e07cbdd25a6d00c0e8fd30b071
MasatoMakino <unagiinu.g@gmail.com>
2020-09-11 15:33:31 +0900

    refactor : demo.js

--- demoSrc/demo.js
+++ demoSrc/demo.js
@@ -2,23 +2,18 @@ import { RAFTicker, RAFTickerEventType } from "../esm";
 
 export class Demo {
   constructor() {
-    RAFTicker.on(RAFTickerEventType.onBeforeTick, (e) => {
-      console.log(
-        `${RAFTickerEventType.onBeforeTick} : ${e.delta}, ${e.timestamp}`
-      );
-    });
-
-    RAFTicker.on(RAFTickerEventType.tick, (e) => {
-      console.log(`${RAFTickerEventType.tick} : ${e.delta}, ${e.timestamp}`);
-    });
-
-    RAFTicker.on(RAFTickerEventType.onAfterTick, (e) => {
-      console.log(
-        `${RAFTickerEventType.onAfterTick} : ${e.delta}, ${e.timestamp}`
-      );
+    const types = Object.values(RAFTickerEventType);
+    types.forEach((type) => {
+      RAFTicker.on(type, (e) => {
+        consoleEvent(type, e);
+      });
     });
   }
 }
+
+const consoleEvent = (type, e) => {
+  console.log(`${type} : ${e.delta}, ${e.timestamp}`);
+};
 
 window.onload = () => {
   const demo = new Demo();
