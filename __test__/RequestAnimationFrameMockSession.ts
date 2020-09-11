a1e95aa2339b14b5d3d99b06e4b04a8a93abd0b1
MasatoMakino <unagiinu.g@gmail.com>
2020-09-11 13:52:12 +0900

    add : licence #4

--- __test__/RequestAnimationFrameMockSession.ts
+++ __test__/RequestAnimationFrameMockSession.ts
@@ -1,17 +1,30 @@
-class RequestAnimationFrameMockSession {
-  handleCounter = 0;
-  queue = new Map();
+/**
+ * Based on : https://stackoverflow.com/a/62282721
+ *
+ * @author : htho https://stackoverflow.com/users/1635906/htho
+ * @licence : CC BY-SA 4.0 https://creativecommons.org/licenses/by-sa/4.0/
+ */
 
-  requestAnimationFrame(callback) {
+class RequestAnimationFrameMockSession {
+  private handleCounter: number = 0;
+  private queue: Map<number, Function> = new Map();
+  private _now: number = 0;
+
+  public now() {
+    return this._now;
+  }
+
+  public requestAnimationFrame(callback: Function) {
     const handle = this.handleCounter++;
     this.queue.set(handle, callback);
     return handle;
   }
-  public cancelAnimationFrame(handle) {
+
+  public cancelAnimationFrame(handle: number): void {
     this.queue.delete(handle);
   }
 
-  private triggerNextAnimationFrame(time) {
+  private triggerNextAnimationFrame(time: number): void {
     const nextEntry = this.queue.entries().next().value;
     if (nextEntry === undefined) return;
 
@@ -20,20 +33,23 @@ class RequestAnimationFrameMockSession {
     cancelAnimationFrame(nextHandle);
   }
 
-  public trigger(time = performance.now()) {
+  public trigger(delta?: number): void {
+    delta ??= 1000 / 60;
+    this._now += delta;
     /**
      * rafのcallbackが自分自身で再登録する場合、無限ループが発生する。
      * そのため固定長のループとする。
      */
     const n = this.queue.size;
     for (let i = 0; i < n; i++) {
-      this.triggerNextAnimationFrame(time);
+      this.triggerNextAnimationFrame(this._now);
     }
   }
 
-  reset() {
+  public reset(): void {
     this.queue.clear();
     this.handleCounter = 0;
+    this._now = 0;
   }
 }
 
@@ -44,5 +60,6 @@ window.requestAnimationFrame = requestAnimationFrameMock.requestAnimationFrame.b
 window.cancelAnimationFrame = requestAnimationFrameMock.cancelAnimationFrame.bind(
   requestAnimationFrameMock
 );
-
-// window.performance.now = requestAnimationFrameMock.now.bind( requestAnimationFrameMock );
+window.performance.now = requestAnimationFrameMock.now.bind(
+  requestAnimationFrameMock
+);
--- __test__/raf.spec.ts
+++ __test__/raf.spec.ts
@@ -12,5 +12,14 @@ describe("raf", () => {
     RAFTicker.on(RAFTickerEventType.tick, mockTick);
     requestAnimationFrameMock.trigger();
     expect(mockTick).toBeCalled();
+    expect(mockTick.mock.results[0].value.delta).toBeCloseTo(1000 / 60);
+    mockTick.mockClear();
+
+    requestAnimationFrameMock.trigger();
+    expect(mockTick).toBeCalled();
+    expect(mockTick.mock.results[0].value.timestamp).toBeCloseTo(
+      (1000 * 2) / 60
+    );
+    expect(mockTick.mock.results[0].value.delta).toBeCloseTo(1000 / 60);
   });
 });
